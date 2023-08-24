cef82a2909dad78e6ad08b0a5b30444b947be3f4
Shoaib Zeshan <szeeshan707@gmail.com>
2023-08-24 19:03:14 +0530

    creating navigation menu with dropdown

---
+++ app.js
@@ -0,0 +1,37 @@
+const dropMenu = document.querySelector('.navbar__dropdownMenu');
+const dropdown_content = document.querySelector('.navbar__dropdown-content');
+const header = document.querySelector('.header')
+const hero = document.querySelector('.hero')
+const nav = document.querySelector('.nav')
+// const body = document.querySelector('document')
+
+console.log(nav)
+
+// dropMenu.addEventListener('mouseover', (e) => {
+//     // console.log(dropdown_content)
+//     dropdown_content.classList.add('navbar__active-drop')
+//     // if ()
+
+//     // console.log('hello')
+// })
+
+// hero.addEventListener('mouseover', (e) => {
+//     dropdown_content.classList.remove('navbar__active-drop')
+//     console.log('first')
+// })
+// header.addEventListener('mouseover', () => {
+//     if (header.classList.contains('navbar__active-drop')) {
+//         dropdown_content.classList.remove('navbar__active-drop')
+
+//     }
+//     console.log('hello')
+// })
+
+// body.addEventListener('mouseover', () => {
+//     if (header.classList.contains('navbar__active-drop')) {
+//         dropdown_content.classList.remove('navbar__active-drop')
+
+//     }
+//     console.log('hello')
+
+// })
