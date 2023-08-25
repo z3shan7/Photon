cbcee8b29e55bcc2ce621cb7f2711aefdf359d4c
Shoaib Zeshan <szeeshan707@gmail.com>
2023-08-25 17:33:55 +0530

    creating sidebar menu

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
-const dropMenu = document.querySelector('.navbar__dropdownMenu');
-const dropdown_content = document.querySelector('.navbar__dropdown-content');
-const header = document.querySelector('.header')
-const hero = document.querySelector('.hero')
-const nav = document.querySelector('.nav')
-// const body = document.querySelector('document')
+const sidebarDropDown = document.getElementById('side-icon')
+const dropDownList = document.querySelector('.sidebar__dropdown-list')
 
-console.log(nav)
+console.log(dropDownList)
 
-// dropMenu.addEventListener('mouseover', (e) => {
-//     // console.log(dropdown_content)
-//     dropdown_content.classList.add('navbar__active-drop')
-//     // if ()
+sidebarDropDown.addEventListener('click', () => {
+    dropDownList.classList.toggle('sidebar__active')
 
-//     // console.log('hello')
-// })
-
-// hero.addEventListener('mouseover', (e) => {
-//     dropdown_content.classList.remove('navbar__active-drop')
-//     console.log('first')
-// })
-// header.addEventListener('mouseover', () => {
-//     if (header.classList.contains('navbar__active-drop')) {
-//         dropdown_content.classList.remove('navbar__active-drop')
-
-//     }
-//     console.log('hello')
-// })
-
-// body.addEventListener('mouseover', () => {
-//     if (header.classList.contains('navbar__active-drop')) {
-//         dropdown_content.classList.remove('navbar__active-drop')
-
-//     }
-//     console.log('hello')
-
-// })
+})
