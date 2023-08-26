4ae79d49873ef611a96076c2f6ee3e065a4e2225
Shoaib Zeshan <szeeshan707@gmail.com>
2023-08-26 20:20:39 +0530

    slider created for gallery

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const sidebarDropDown = document.getElementById('side-icon')
 const dropDownList = document.querySelector('.sidebar__dropdown-list')
 
-console.log(dropDownList)
 
 sidebarDropDown.addEventListener('click', () => {
     dropDownList.classList.toggle('sidebar__active')
 
+})
+
+const hamIcon = document.querySelector('.menu__icon')
+const sidebar = document.querySelector('.sidebar')
+const closeIcon = document.querySelector('.sidebar__closeBtn')
+
+console.log(sidebar)
+
+hamIcon.addEventListener('click', () => {
+    sidebar.classList.add('sidebar__show')
+
+
+})
+closeIcon.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar__show')
 })
